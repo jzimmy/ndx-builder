47c2a52bfe5fc2b7a4c8104798fb419be1b4ec40
Jacob Zimmerman <jmzimmerman@lbl.gov>
2023-07-03 10:56:51 -0700

    demo deploy

--- src/components/type-builder/typedef.ts
+++ src/components/type-builder/typedef.ts
@@ -1,5 +1,5 @@
 import { LitElement, html, css } from "lit";
-import { customElement, property, state } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
 
 @customElement("typedef-constructor")
 export class TypedefConstructor extends LitElement {
